47d00272acf8598442d819ef057933f0c14cc9bc
Thanh Nguyen <btnguyen2k@gmail.com>
2015-07-10 23:43:26 +0700

    Minor update the demo UI

--- Swift-RSAUtils/RSAUtils.swift
+++ Swift-RSAUtils/RSAUtils.swift
@@ -5,5 +5,236 @@
 //  Created by Thanh Nguyen on 7/10/15.
 //  Copyright (c) 2015 Thanh Nguyen. All rights reserved.
 //
+////////////////////////////////////////////////////////////////////////
+// RSA Utility Class
+// Credits:
+// - https://github.com/ideawu/Objective-C-RSA
+// - http://netsplit.com/swift-storing-key-pairs-in-the-keyring
+// - http://netsplit.com/swift-generating-keys-and-encrypting-and-decrypting-text
 
 import Foundation
+import Security
+
+public class RSAUtils: NSObject {
+    // Base64 encode a block of data
+    static private func base64Encode(data: NSData) -> String {
+        return data.base64EncodedStringWithOptions(nil)
+    }
+    
+    // Base64 decode a base64-ed string
+    static private func base64Decode(strBase64: String) -> NSData {
+        let data = NSData(base64EncodedString: strBase64, options: nil)
+        return data!
+    }
+    
+    // Verify that the supplied key is in fact a PKCS#1 key and strip the header
+    static public func stripPublicKeyHeader(pubkey: NSData) -> NSData? {
+        if ( pubkey.length == 0 ) {
+            return nil
+        }
+        
+        var keyAsArray = [UInt8](count: pubkey.length / sizeof(UInt8), repeatedValue: 0)
+        pubkey.getBytes(&keyAsArray, length: pubkey.length)
+        
+        var idx = 0
+        if (keyAsArray[idx++] != 0x30) {
+            return nil
+        }
+        if (keyAsArray[idx] > 0x80) {
+            idx += Int(keyAsArray[idx]) - 0x80 + 1
+        } else {
+            idx++
+        }
+        
+        let seqiod = [UInt8](arrayLiteral: 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00)
+        for ( var i = idx; i < idx+15; i++ ) {
+            if ( keyAsArray[i] != seqiod[i-idx] ) {
+                return nil
+            }
+        }
+        idx += 15
+        
+        if (keyAsArray[idx++] != 0x03) {
+            return nil
+        }
+        if (keyAsArray[idx] > 0x80) {
+            idx += Int(keyAsArray[idx]) - 0x80 + 1;
+        } else {
+            idx++
+        }
+        
+        if (keyAsArray[idx++] != 0x00) {
+            return nil
+        }
+        
+        var newArray = [UInt8](count: (keyAsArray.count - idx) / sizeof(UInt8), repeatedValue: 0)
+        for ( var i = idx; i < keyAsArray.count; i++ ) {
+            newArray[i-idx] = keyAsArray[i]
+        }
+        
+        return NSData(bytes: newArray, length: newArray.count)
+    }
+    
+    // Get a SecKeyRef from keychain
+    static public func getRSAKeyFromKeychain(tagName: String) -> SecKeyRef? {
+        var queryFilter: [String: AnyObject] = [
+            String(kSecClass)             : kSecClassKey,
+            String(kSecAttrKeyType)       : kSecAttrKeyTypeRSA,
+            String(kSecAttrApplicationTag): tagName,
+            String(kSecReturnRef)         : true
+        ]
+        var keyPtr: Unmanaged<AnyObject>?
+        var result = SecItemCopyMatching(queryFilter, &keyPtr)
+        if ( result != noErr || keyPtr == nil ) {
+            return nil
+        }
+        return (keyPtr!.takeRetainedValue() as! SecKeyRef)
+    }
+    
+    // Add a pubic key to keychain and return its SecKeyRef
+    static public func addPublicKey(pubkeyBase64: String, tagName: String) -> SecKeyRef? {
+        return addPublicKey(pubkey: base64Decode(pubkeyBase64), tagName: tagName)
+    }
+    
+    static private func addPublicKey(#pubkey: NSData, tagName: String) -> SecKeyRef? {
+        var pubkeyData = stripPublicKeyHeader(pubkey)
+        if ( pubkeyData == nil ) {
+            return nil
+        }
+        
+        // Delete any old lingering key with the same tag
+        var queryFilter: [String: AnyObject] = [
+            String(kSecClass)             : kSecClassKey,
+            String(kSecAttrKeyType)       : kSecAttrKeyTypeRSA,
+            String(kSecAttrApplicationTag): tagName
+        ]
+        var result = SecItemDelete(queryFilter)
+        
+        var keyAsArray = [UInt8](count: pubkeyData!.length / sizeof(UInt8), repeatedValue: 0)
+        pubkeyData!.getBytes(&keyAsArray, length: pubkeyData!.length)
+        
+        // Add persistent version of the key to system keychain
+        //var prt1: Unmanaged<AnyObject>?
+        queryFilter = [
+            String(kSecClass)              : kSecClassKey,
+            String(kSecAttrKeyType)        : kSecAttrKeyTypeRSA,
+            String(kSecAttrApplicationTag) : tagName,
+            String(kSecValueData)          : pubkeyData!,
+            String(kSecAttrKeyClass)       : kSecAttrKeyClassPublic,
+            String(kSecReturnPersistentRef): true
+        ]
+        result = SecItemAdd(queryFilter, nil)
+        if ((result != noErr) && (result != errSecDuplicateItem)) {
+            return nil
+        }
+        
+        return getRSAKeyFromKeychain(tagName)
+    }
+    
+    // Encrypts data with a public key
+    static public func encryptWithPublicKey(data: NSData, pubkeyBase64: String, keychainTag: String) -> NSData? {
+        var keyRef = getRSAKeyFromKeychain(keychainTag)
+        if ( keyRef == nil ) {
+            keyRef = addPublicKey(pubkeyBase64, tagName: keychainTag)
+        }
+        if ( keyRef == nil ) {
+            return nil
+        }
+        
+        let blockSize = SecKeyGetBlockSize(keyRef!)
+        let maxChunkSize = blockSize - 11
+        
+        var decryptedDataAsArray = [UInt8](count: data.length / sizeof(UInt8), repeatedValue: 0)
+        data.getBytes(&decryptedDataAsArray, length: data.length)
+        
+        var encryptedData = [UInt8](count: 0, repeatedValue: 0)
+        var idx = 0
+        while (idx < decryptedDataAsArray.count ) {
+            var idxEnd = idx + maxChunkSize
+            if ( idxEnd > decryptedDataAsArray.count ) {
+                idxEnd = decryptedDataAsArray.count
+            }
+            var chunkData = [UInt8](count: maxChunkSize, repeatedValue: 0)
+            for ( var i = idx; i < idxEnd; i++ ) {
+                chunkData[i-idx] = decryptedDataAsArray[i]
+            }
+            
+            var encryptedDataBuffer = [UInt8](count: blockSize, repeatedValue: 0)
+            var encryptedDataLength = blockSize
+            
+            let status = SecKeyEncrypt(keyRef!, SecPadding(kSecPaddingPKCS1), chunkData, idxEnd-idx, &encryptedDataBuffer, &encryptedDataLength)
+            if ( status != noErr ) {
+                return nil
+            }
+            //let finalData = removePadding(encryptedDataBuffer)
+            encryptedData += encryptedDataBuffer
+            
+            idx += maxChunkSize
+        }
+        
+        return NSData(bytes: encryptedData, length: encryptedData.count)
+    }
+    
+    // Decrypt an encrypted data with a public key
+    static public func decryptWithPublicKey(encryptedData: NSData, pubkeyBase64: String, keychainTag: String) -> NSData? {
+        var keyRef = getRSAKeyFromKeychain(keychainTag)
+        if ( keyRef == nil ) {
+            keyRef = addPublicKey(pubkeyBase64, tagName: keychainTag)
+        }
+        if ( keyRef == nil ) {
+            return nil
+        }
+        
+        let blockSize = SecKeyGetBlockSize(keyRef!)
+        
+        var encryptedDataAsArray = [UInt8](count: encryptedData.length / sizeof(UInt8), repeatedValue: 0)
+        encryptedData.getBytes(&encryptedDataAsArray, length: encryptedData.length)
+
+        var decryptedData = [UInt8](count: 0, repeatedValue: 0)
+        var idx = 0
+        while (idx < encryptedDataAsArray.count ) {
+            var idxEnd = idx + blockSize
+            if ( idxEnd > encryptedDataAsArray.count ) {
+                idxEnd = encryptedDataAsArray.count
+            }
+            var chunkData = [UInt8](count: blockSize, repeatedValue: 0)
+            for ( var i = idx; i < idxEnd; i++ ) {
+                chunkData[i-idx] = encryptedDataAsArray[i]
+            }
+            
+            var decryptedDataBuffer = [UInt8](count: blockSize, repeatedValue: 0)
+            var decryptedDataLength = blockSize
+            
+            let status = SecKeyDecrypt(keyRef!, SecPadding(kSecPaddingNone), chunkData, idxEnd-idx, &decryptedDataBuffer, &decryptedDataLength)
+            if ( status != noErr ) {
+                return nil
+            }
+            let finalData = removePadding(decryptedDataBuffer)
+            decryptedData += finalData
+            
+            idx += blockSize
+        }
+        
+        return NSData(bytes: decryptedData, length: decryptedData.count)
+    }
+    
+    static private func removePadding(data: [UInt8]) -> [UInt8] {
+        var idxFirstZero = -1
+        var idxNextZero = data.count
+        for ( var i = 0; i < data.count; i++ ) {
+            if ( data[i] == 0 ) {
+                if ( idxFirstZero < 0 ) {
+                    idxFirstZero = i
+                } else {
+                    idxNextZero = i
+                    break
+                }
+            }
+        }
+        var newData = [UInt8](count: idxNextZero-idxFirstZero-1, repeatedValue: 0)
+        for ( var i = idxFirstZero+1; i < idxNextZero; i++ ) {
+            newData[i-idxFirstZero-1] = data[i]
+        }
+        return newData
+    }
+}
--- Swift-RSAUtils/ViewController.swift
+++ Swift-RSAUtils/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var txtTextToEncrypt: UITextField!
+    
+    @IBOutlet weak var txtEncryptedTextPubKey: UITextField!
+    
+    // RSA public key in base64 / Key length: 512 bits
+    let PUBLIC_KEY = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJh+/sdLdlVVcM5V5/j/RbwM8SL++Sc3dMqMK1nP73XYKhvO63bxPkWwaY0kwcUU40+QducwjueVOzcPFvHf+fECAwEAAQ=="
+    
+    // RSA private key in base64 / Key length: 512 bits
+    let PRIVATE_KEY = "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAmH7+x0t2VVVwzlXn+P9FvAzxIv75Jzd0yowrWc/vddgqG87rdvE+RbBpjSTBxRTjT5B25zCO55U7Nw8W8d/58QIDAQABAkBCNqIZlsKCut6IOPTIQM7eoB/zuhIk3QdxCvunu4mV+OIv00b6lN02ZsQ64nblu6dP9UuhlyclFaGlXtwqfkABAiEA0XQlb0mT5cZ8VpNNOqojeWoyrvQIRPGhdBrq3VroT4ECIQC6YoVd0yaT6lUDV+tgKtNbQN8m9hVIMgE/awRT/aXicQIhAK+jIbEMlgTcSG+g3eYPveeWciHbaQPHS4g8+i3ciWoBAiBddJsEwaQ9VKlN5N67uJ2DyxJZediP+6rOfr2L08pCsQIhAJLmeidBF0uJxNZiBgnkIHlRQ167qE1D0s5SQ2j5217G"
+    
+    // Sample text: "Thanh Nguyen - This is expected to be a longer-than-53-characters text"
+    
+    // Sample text, encrypted with public key
+    let DATA_ENCRYPTED_WITH_PUBLIC_KEY = "NDgglMK2r3kp3/F6HAOPF8c571k6mqHdJ61lx6rTcX/gOvuo/xduB05kUvDBUbvTPNGRKUOamNv1OaJv1til+AOUjI8whuCZqceq8qvVqN0lQpv+c2vYL7LotrCYioZSybzyYbhkevqYtRm4CMk+T4pUS4tCmQxL3dK2nFJl9GE="
+
+    // Sample text, encrypted with private key
+    let DATA_ENCRYPTED_WITH_PRIVATE_KEY = "eSeAcLggg5IYrGnA3JRgHB5jjEU3IlPce0q+1PM80Gzv6Fi722DH1DOZ3DUpgoQY2ZADBbRERdYSa+hxH9wDARtfpJn3huCAvc1e0p+G2tQ44GuXpC0CoYyeQ4MB1UH8zrPdL6NryQ2zSFp0JAt9u/6ZQWqE+fyafc+Zs68GumQ="
+    
+    // tag name to access the stored private key stored in keychain
+    let TAG_PRIVATE_KEY = "com.github.btnguyen2k.Sample_Private"
+
+    // tag name to access the stored public key in keychain
+    let TAG_PUBLIC_KEY = "com.github.btnguyen2k.Sample_Public"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        txtEncryptedTextPubKey.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func doEncrypt() {
+        txtEncryptedTextPubKey.text = ""
+        
+        let textToEncrypt = txtTextToEncrypt.text
+        if ( textToEncrypt == nil || textToEncrypt == "" ) {
+            txtEncryptedTextPubKey.text = "Please enter some text to encrypt"
+            txtEncryptedTextPubKey.textColor = UIColor.redColor()
+            return
+        }
+        
+        var encryptedData = RSAUtils.encryptWithPublicKey((textToEncrypt as NSString).dataUsingEncoding(NSUTF8StringEncoding)!, pubkeyBase64: PUBLIC_KEY, keychainTag: TAG_PUBLIC_KEY)
+        if ( encryptedData == nil ) {
+            txtEncryptedTextPubKey.text = "Error while encrypting"
+            txtEncryptedTextPubKey.textColor = UIColor.redColor()
+            return
+        }
+        var encryptedDataText = encryptedData!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.allZeros)
+        println("Encrypt with pubkey: \(encryptedDataText)")
+        txtEncryptedTextPubKey.text = encryptedDataText
+        txtEncryptedTextPubKey.textColor = UIColor.blueColor()
+     
+//        let DATA = "aFhPGWbA5WCBfewPsx0AaxPUyipIrEJrTxS4FMKXmmcb2r/Mq6X1eLQ14eYhWi3Q3ViHvd7dA9atVbct/iXMvg=="
+        let DATA = "eSeAcLggg5IYrGnA3JRgHB5jjEU3IlPce0q+1PM80Gzv6Fi722DH1DOZ3DUpgoQY2ZADBbRERdYSa+hxH9wDARtfpJn3huCAvc1e0p+G2tQ44GuXpC0CoYyeQ4MB1UH8zrPdL6NryQ2zSFp0JAt9u/6ZQWqE+fyafc+Zs68GumQ="
+        encryptedData = NSData(base64EncodedString: DATA, options: NSDataBase64DecodingOptions.allZeros)
+        let decryptedData = RSAUtils.decryptWithPublicKey(encryptedData!, pubkeyBase64: PUBLIC_KEY, keychainTag: TAG_PUBLIC_KEY)
+        if ( decryptedData != nil ) {
+            println(NSString(data: decryptedData!, encoding:NSUTF8StringEncoding))
+        }
+    }
+    
 }
 
